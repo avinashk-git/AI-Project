9515a03607a0a051c9435331530f5fe2533f0146
avinashaar <avinashkt97@gmail.com>
2024-03-25 06:51:05 +0100

    about convolution

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-
-print("avinash")
+numbers=[30,10,-45,5,20]
+print(numbers)
+numbers.sort(reverse=True)
+numbers2=numbers
+numbers2[2]
+print(numbers2[0:4:2])
+print(numbers)
+print(f"second largest number {numbers[1]}")
